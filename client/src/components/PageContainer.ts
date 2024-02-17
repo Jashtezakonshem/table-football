6dc8014cb687734fa2b73fe0c6bdcb9890e40f04
oltioff <oltion.abazi@switcho.it>
2024-02-17 17:41:13 +0100

    feat(Dashboard): create first route with wouter

---
+++ client/src/components/PageContainer.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const PageContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  flex-direction: column;
+  display: flex;
+`;
+
+export { PageContainer };
---
+++ client/src/pages/Dashbboard.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { PageContainer } from "../components/PageContainer";
+export const Dashboard = () => {
+  return (
+    <PageContainer>
+      <h1>Welcome to the Dashboard!</h1>
+      <p>This is a protected route.</p>
+    </PageContainer>
+  );
+};
