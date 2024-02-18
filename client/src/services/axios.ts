03f37db0231d37c3429c891ed028ebcfe3af6e7d
oltioff <oltion.abazi@switcho.it>
2024-02-18 17:16:12 +0100

    feat(axios): add interceptor for notification

--- client/src/services/axios.ts
+++ client/src/services/axios.ts
@@ -1,5 +1,15 @@
 import axios from "axios";
+import { notification } from "antd";
 
 axios.defaults.baseURL = "http://localhost:8080/";
 axios.defaults.headers.common["Content-Type"] = "application/json";
 // I would use axios interceptor to add the token to the request header in a real world scenario
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.log(error);
+    notification.error({
+      message: error.response?.data?.error || "Error",
+    });
+  },
+);
